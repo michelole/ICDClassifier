d8dc56787ad55502a8bb3bc1b9e50fe45c7e8600
Michel Oleynik <michel@ime.usp.br>
2016-10-24 15:47:12 +0200

    Corrects HSQLDB file location.

--- hsqldbView.sh
+++ hsqldbView.sh
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass="org.hsqldb.util.DatabaseManagerSwing" -Dexec.args="--url jdbc:hsqldb:file:target/db/icdc"
+mvn exec:java -Dexec.mainClass="org.hsqldb.util.DatabaseManagerSwing" -Dexec.args="--url jdbc:hsqldb:file:src/db/icdc"
