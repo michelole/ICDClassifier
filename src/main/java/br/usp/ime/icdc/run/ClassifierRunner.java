b92c09872afbb8b55321ce6ce35a47354ee7f500
Michel Oleynik <michelole@gmail.com>
2016-11-03 19:50:15 +0100

    Improves statistics generation.
    - Print additional statistical data.
    - Remove unused and commented code
    - Enable stopwords to speed up cross-validation.

--- src/main/java/br/usp/ime/icdc/run/ClassifierRunner.java
+++ src/main/java/br/usp/ime/icdc/run/ClassifierRunner.java
@@ -1,7 +1,6 @@
 package br.usp.ime.icdc.run;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -38,20 +37,6 @@ public class ClassifierRunner {
 
 		final List<Configuration> configs = new ArrayList<Configuration>();
 
-		// Targets[] targets = new Targets[1];
-		// targets[0] = Targets.TOPOGRAPHY_GROUP;
-		// targets[1] = Targets.MORPHOLOGY_GROUP;
-		// targets[2] = Targets.TOPOGRAPHY_CATEGORY;
-		// targets[3] = Targets.TOPOGRAPHY;
-		// targets[4] = Targets.MORPHOLOGY;
-
-		// for (Targets t : targets)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.NAIVE, SmoothingTechniques.ADD_ONE, Sources.ALL, false,
-		// SentenceDetectors.NONE, Tokenizers.WORD, Stemmers.NONE,
-		// Chunkers.NONE, t, 1, -1,
-		// MetastasisStatus.NONM1));
-
 		Set<Sections> sections = new HashSet<Sections>();
 		sections.add(Sections.MACROSCOPY);
 		sections.add(Sections.MICROSCOPY);
@@ -61,94 +46,19 @@ public class ClassifierRunner {
 		sections.add(Sections.LIQUID_CYTOPATHOLOGY);
 
 		double cost;
-		for (int i = -11; i <= -3; i += 4) {
-			cost = Math.pow(2, i);
+		int power[] = {-7, -3, -11};
+		for (int p : power) {
+			cost = Math.pow(2, p);
 			configs.add(new Configuration(sections, Criteria.MANY_REPORT_ONE_REGISTRY, Classifiers.SVM,
-					WeightingSchemes.TFIDF, SmoothingTechniques.ADD_ONE, cost, Sources.ALL, false, 2,
+					WeightingSchemes.TFIDF, SmoothingTechniques.ADD_ONE, cost, Sources.ALL, true, 2,
 					SentenceDetectors.NONE, Tokenizers.WORD, Stemmers.NONE, Chunkers.NONE, Targets.TOPOGRAPHY_GROUP, 1,
 					-1, MetastasisStatus.NONM1));
 
 		}
 
-		// FIXME java.lang.IllegalArgumentException: Can't have more folds than
-		// instances!
-		// N = 3?
-		// sections = new HashSet<Sections>();
-		// sections.add(Sections.CYTOPATHOLOGY);
-		// configs.add(new Configuration(sections,
-		// Criteria.MANY_REPORT_ONE_REGISTRY, Classifiers.BAYES,
-		// SmoothingTechniques.ADD_ONE, Sources.ALL, false,
-		// SentenceDetectors.NONE, Tokenizers.WORD, Stemmers.NONE,
-		// Chunkers.NONE, Targets.TOPOGRAPHY_GROUP, 1, -1,
-		// MetastasisStatus.NONM1));
-
-		// FIXME java.lang.IllegalArgumentException: Can't have more folds than
-		// instances!
-		// sections = new HashSet<Sections>();
-		// sections.add(Sections.LIQUID_CYTOPATHOLOGY);
-		// configs.add(new Configuration(sections,
-		// Criteria.MANY_REPORT_ONE_REGISTRY, Classifiers.BAYES,
-		// SmoothingTechniques.ADD_ONE, Sources.ALL, false,
-		// SentenceDetectors.NONE, Tokenizers.WORD, Stemmers.NONE,
-		// Chunkers.NONE, Targets.TOPOGRAPHY_GROUP, 1, -1,
-		// MetastasisStatus.NONM1));
-
-		// for (Targets t : targets)
-		// for (int i = 1; i <= 30; i++)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.BAYES, SmoothingTechniques.ADD_ONE, Sources.ALL, false,
-		// SentenceDetectors.NONE, Tokenizers.WORD, Stemmers.NONE,
-		// Chunkers.NONE, t, i, -1,
-		// MetastasisStatus.NONM1));
-
-		/* Suavization */
-		// for (Targets t : targets)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.BAYES, SmoothingTechniques.GOOD_TURING, Sources.ALL,
-		// false,
-		// SentenceDetectors.NONE, Tokenizers.WORD, Stemmers.NONE,
-		// Chunkers.NONE, t, 1, -1,
-		// MetastasisStatus.NONM1));
-
-		// for (Targets t : targets)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.BAYES, SmoothingTechniques.ADD_ALPHA, Sources.ALL, false,
-		// SentenceDetectors.NONE, Tokenizers.WORD, Stemmers.NONE,
-		// Chunkers.NONE, t, 1, -1,
-		// MetastasisStatus.NONM1));
-
-		/* NLP */
-		// for (Targets t : targets)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.BAYES, SmoothingTechniques.ADD_ALPHA, Sources.ALL, true,
-		// SentenceDetectors.COGROO, Tokenizers.COGROO, Stemmers.NONE,
-		// Chunkers.NONE, t, 1, -1,
-		// MetastasisStatus.NONM1));
-
-		// for (Targets t : targets)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.BAYES, SmoothingTechniques.ADD_ALPHA, Sources.ALL, true,
-		// SentenceDetectors.COGROO, Tokenizers.COGROO, Stemmers.PORTER,
-		// Chunkers.NONE, t, 1, -1,
-		// MetastasisStatus.NONM1));
-
-		// for (Targets t : targets)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.BAYES, SmoothingTechniques.ADD_ALPHA, Sources.ALL, true,
-		// SentenceDetectors.COGROO, Tokenizers.COGROO, Stemmers.COGROO,
-		// Chunkers.NONE, t, 1, -1,
-		// MetastasisStatus.NONM1));
-
-		// for (Targets t : targets)
-		// configs.add(new Configuration(Criteria.MANY_REPORT_ONE_REGISTRY,
-		// Classifiers.BAYES, SmoothingTechniques.ADD_ALPHA, Sources.ALL, true,
-		// SentenceDetectors.COGROO, Tokenizers.COGROO, Stemmers.COGROO,
-		// Chunkers.COGROO, t, 1, -1,
-		// MetastasisStatus.NONM1));
-
 		ExperimentalCipeClassifier c;
 
-		CSVWriter writer = ExperimentalCipeClassifier.getWriter("evaluation.csv");
+		CSVWriter writer = ExperimentalCipeClassifier.getWriter(Constants.CONFIG.getStringRepresentation() + ".csv");
 
 		for (Configuration config : configs) {
 			Constants.CONFIG = config;
@@ -157,7 +67,8 @@ public class ClassifierRunner {
 			LOG.info(config.getStringRepresentation());
 			c = new ExperimentalCipeClassifier();
 
-			// c.printCorpusStats();
+			LOG.info("Let's print corpus stats to a file.");
+			c.printCorpusStats();
 
 			// LOG.info("Single evaluation.");
 			// c.singleEvaluate();
@@ -169,11 +80,11 @@ public class ClassifierRunner {
 			c.crossValidate();
 			c.printStatsAsCsvLine(writer, Double.toString(config.getSvmCostParameter()));
 
-			// LOG.info("Let's print stats to a file.");
-			// c.printStats();
+			LOG.info("Let's print stats to a file.");
+			c.printStats();
 			//
-			// LOG.info("Let's print classifier details to a file.");
-			// c.printClassifier();
+			LOG.info("Let's print classifier details to a file.");
+			c.printClassifier();
 
 			// LOG.info("Increasing words.");
 			// c.increasingWords();
